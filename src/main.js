85d88a57abd03b0cb493a6c796079267ba6e0517
hitesh sunkara <hiteshsunkara@eworldes.com>
2023-07-07 01:50:17 +0530

    env status

--- src/main.js
+++ src/main.js
@@ -4,6 +4,15 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 import { Quasar } from 'quasar'
+import { createHead } from "@vueuse/head"
 import quasarUserOptions from './quasar-user-options'
 
-createApp(App).use(Quasar, quasarUserOptions).use(store).use(router).mount('#app')
+const SW = createApp(App)
+const Head = createHead()
+
+SW
+    .use(Head)
+    .use(Quasar, quasarUserOptions)
+    .use(store)
+    .use(router)
+    .mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <SigmaSeo title="SigmaWings | The best place for sell your products" description="SigmaWings | we help you in selling products" keywords="hello sigma"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import HelloWorld from "@/components/HelloWorld.vue";
+import SigmaSeo from "../components/Head/Seo.vue"
 export default {
-  name: 'HomeView',
+  name: "HomeView",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+    SigmaSeo
+  },
+};
 </script>
