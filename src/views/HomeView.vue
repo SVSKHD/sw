73645af75fceb5e239dbc3b8f40b445c3e3b5f6c
hitesh sunkara <hiteshsunkara@eworldes.com>
2023-07-09 01:40:05 +0530

    layout

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,16 @@
 <template>
   <div class="home">
     <SigmaSeo title="SigmaWings | The best place for sell your products" description="SigmaWings | we help you in selling products" keywords="hello sigma"/>
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1 class="text-center">Hello Sigma</h1>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 import SigmaSeo from "../components/Head/Seo.vue"
 export default {
   name: "HomeView",
   components: {
-    HelloWorld,
     SigmaSeo
   },
 };
